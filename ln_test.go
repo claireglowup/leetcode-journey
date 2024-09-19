d4316d152daaab6670340b96153b36fd00eb6c0d
claireglowup <claireglowup@gmail.com>
2024-09-20 05:46:51 +0700

    fix largest number

--- ln_test.go
+++ ln_test.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
 	"testing"
 )
 
 func TestLargestNumber(t *testing.T) {
 
-	input := []int{3, 30, 34, 5, 9}
-	expect := "9534330"
+	// nums := []int{3, 30, 34, 5, 9} "9534330"
+	nums := []int{2, 10} //210
 
-	for i := 0; i < len(input); i++ {
+	for i := 1; i < len(nums); i++ {
+		cek, _ := strconv.Atoi(strconv.Itoa(nums[i-1]) + strconv.Itoa(nums[i]))
+		cek2, _ := strconv.Atoi(strconv.Itoa(nums[i]) + strconv.Itoa(nums[i-1]))
+
+		if cek < cek2 {
+			nums[i] = cek2
+		} else {
+			nums[i] = cek
+
+		}
 
 	}
+	fmt.Println(nums[len(nums)-1])
 
 }
