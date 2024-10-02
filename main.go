664a0300c95bfd6cf42e8ef1c96d438d2c26df16
claireglowup <claireglowup@gmail.com>
2024-10-03 00:33:19 +0700

    still confuse

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	arr := []int{1, 2, 3, 4, 5, 10, 6, 7, 8, 9}
+	k := 5
+
+	var result [][]int
+	remainderMap := make(map[int]int) // Map untuk menyimpan sisa pembagian
+
+	for _, each := range arr {
+		rem := each % k
+		complement := (k - rem) % k // Cari pasangan yang melengkapi jadi kelipatan k
+
+		if remainderMap[complement] > 0 {
+			result = append(result, []int{each, complement * k})
+			remainderMap[complement]-- // Kurangi jumlah pasangan yang tersedia
+		} else {
+			remainderMap[rem]++ // Tambahkan elemen ke map
+		}
+	}
+
+	fmt.Println(result)
 }
